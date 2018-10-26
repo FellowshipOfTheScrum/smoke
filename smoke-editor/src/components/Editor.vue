2fafc0dfd77afd0dc023994100e0637689389dc0
AdamSchaible <31319844+AdamSchaible@users.noreply.github.com>
2018-10-26 15:18:02 -0600

    Rough draft of adding resizing feature to editor

--- smoke-editor/src/components/Editor.vue
+++ smoke-editor/src/components/Editor.vue
@@ -1,28 +1,56 @@
 <template>
-  <div class = "editor">
-    <b-dropdown v-model="selectedLanguage">
-      <button slot="trigger" class="button is-primary">
-        <span>{{ selectedLanguage.name }}</span>
-        <b-icon icon="arrow-down"/>
-      </button>
+  <section>
+    <div class = "columns"> <!--Starts columns-->
+      <div class = "column"> <!--Left Auto column-->
+        <!--This makes the left side column Auto-Adjust to shrink or expand-->
+      </div>
 
-      <b-dropdown-item v-for="lang in languages"
-                       v-bind:value="lang"
-                       v-bind:key="lang.name">
-                       {{ lang.name }}
-      </b-dropdown-item>
-    </b-dropdown>
+      <!--"is-half" was added to make editor half window-->
+      <!--when it is "is-full" the button goes to far right into margin-->
+      <div class = "column is-half"> <!--Right column-->
+        <div class = "editor">
+          <b-dropdown v-model="selectedLanguage">
+            <button slot="trigger" class="button is-primary">
+              <span>{{ selectedLanguage.name }}</span>
+              <b-icon icon="arrow-down"/>
+            </button>
 
-    <brace style="height: 500px"
-           :fontsize="'12px'"
-           :theme="'xcode'"
-           :mode="selectedLanguage.tag"
-           :codefolding="'markbegin'"
-           :softwrap="'free'"
-           :selectionstyle="'text'"
-           :highlightline="true">
-    </brace>
-  </div>
+            <b-dropdown-item v-for="lang in languages"
+                             v-bind:value="lang"
+                             v-bind:key="lang.name">
+              {{ lang.name }}
+            </b-dropdown-item>
+          </b-dropdown>
+
+          <!--This is the Resize button-->
+          <div class = "button is-small is-warning is-pulled-right">
+                <span class="icon">
+                  <i class="fas window-minimize"></i> <!--Somehow doesn't show the icon-->
+                </span>
+          </div>
+
+          <!--Here is another Resize button-->
+          <div class="button is-small is-pulled-right is-rounded is-outlined is-danger">
+            <input id="switchExample" type="checkbox" name="switchExample" class="switch" checked="checked">
+          </div>
+
+          <!--I added width, might need to add right margin-->
+          <brace style="height: 500px; width: 1400px"
+                 :fontsize="'12px'"
+                 :theme="'github'"
+                 :mode="selectedLanguage.tag"
+                 :codefolding="'markbegin'"
+                 :softwrap="'free'"
+                 :selectionstyle="'text'"
+                 :highlightline="true">
+          </brace>
+        </div>
+      </div>
+
+    </div>
+
+    </div>
+  </section>
 </template>
 
 
